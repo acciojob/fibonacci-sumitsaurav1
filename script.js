9181dfe0c5c7f5de40c813017b48c9127675628e
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-15 12:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function fibonacci(num) {
 		return 1;
 	}
 	let a=[];
+     a[0]=0;
+     a[1]=1;
 	for(let i=2;i<=n;i++){
 		a[i]=a[i-1]+a[i-2];
 	}
-	return a[n];
+	return a[n-1];
 	// your code here
 }
 
