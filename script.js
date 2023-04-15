2b86d4ca572fe725347ffa69f55f39e5dda51d2c
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-15 12:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+   if(n==1)
+   {
+	   return 0;
+   }
+	if(n==2){
+		return 1;
+	}
+	let a=[];
+	for(let i=2;i<=n;i++){
+		a[i]=a[i-1]+a[i-2];
+	}
+	return a[n];
+	// your code here
 }
 
 module.exports = fibonacci;
